f094372d68d27b01237855fde843e7065aed9a2d
Alon Zakai <alonzakai@gmail.com>
2012-04-07 21:45:26 -0700

    set forceglsl to 1

--- cube2/src/engine/shader.cpp
+++ cube2/src/engine/shader.cpp
@@ -744,7 +744,7 @@ void Shader::bindprograms()
 
 VARFN(shaders, useshaders, -1, -1, 1, initwarning("shaders"));
 VARF(shaderprecision, 0, 0, 2, initwarning("shader quality"));
-VARF(forceglsl, 0, 0, 1, initwarning("shaders"));
+VARF(forceglsl, 0, 1, 1, initwarning("shaders"));
 
 bool Shader::compile()
 {
