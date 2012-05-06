44d4d1aa10f91af57988b2a5c9e93e05ad864a24
Alon Zakai <alonzakai@gmail.com>
2012-05-06 12:19:34 -0700

    default size to 800x600

--- cube2/src/engine/main.cpp
+++ cube2/src/engine/main.cpp
@@ -92,8 +92,8 @@ bool initwarning(const char *desc, int level, int type)
 #define SCR_MINH 200
 #define SCR_MAXW 10000
 #define SCR_MAXH 10000
-#define SCR_DEFAULTW 1024
-#define SCR_DEFAULTH 768
+#define SCR_DEFAULTW 800
+#define SCR_DEFAULTH 600
 VARF(scr_w, SCR_MINW, -1, SCR_MAXW, initwarning("screen resolution"));
 VARF(scr_h, SCR_MINH, -1, SCR_MAXH, initwarning("screen resolution"));
 VARF(colorbits, 0, 0, 32, initwarning("color depth"));
