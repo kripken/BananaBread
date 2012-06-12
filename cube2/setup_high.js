4e5d327026c64d4b75c96cb57f15e1e4324f9425
Alon Zakai <alonzakai@gmail.com>
2012-06-12 10:30:05 -0700

    generate low, medium and high builds

---
+++ cube2/setup_high.js
@@ -0,0 +1,8 @@
+
+Module['setPlayerModels'] = function() {
+  var setPlayerModelInfo = Module['cwrap']('_ZN4game18setplayermodelinfoEPKcS1_S1_S1_S1_S1_S1_S1_S1_S1_S1_S1_b', null,
+    ['string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'number']);
+
+  setPlayerModelInfo("snoutx10k", "snoutx10k", "snoutx10k", "snoutx10k/hudguns", 0, 0, 0, 0, 0, "snoutx10k", "snoutx10k", "snoutx10k", true);
+}
+
---
+++ cube2/setup_medium.js
@@ -0,0 +1,8 @@
+
+Module['setPlayerModels'] = function() {
+  var setPlayerModelInfo = Module['cwrap']('_ZN4game18setplayermodelinfoEPKcS1_S1_S1_S1_S1_S1_S1_S1_S1_S1_S1_b', null,
+    ['string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'number']);
+
+  setPlayerModelInfo("frankie", "frankie", "frankie", "snoutx10k/hudguns", "nada", 0, 0, 0, 0, "frankie", "frankie", "frankie", false);
+}
+
