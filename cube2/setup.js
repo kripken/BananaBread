b5b56d71fe32fc166e523081fa415b56da7ce5b2
Alon Zakai <alonzakai@gmail.com>
2012-07-28 18:17:48 -0700

    move Module definition into setup.js

--- cube2/setup.js
+++ cube2/setup.js
@@ -26,6 +26,69 @@
   if (!pointerLock) fail('pointer lock/mouse lock');
 })();
 
+// Setup compiled code parameters and interaction with the web page
+var Module = {
+  preRun: [],
+  postRun: [],
+  preloadPlugins: [],
+  print: (function() {
+    var element = document.getElementById('output');
+    element.value = ''; // clear browser cache
+    return function(text) {
+      // These replacements are necessary if you render to raw HTML
+      //text = text.replace(/&/g, "&amp;");
+      //text = text.replace(/</g, "&lt;");
+      //text = text.replace(/>/g, "&gt;");
+      //text = text.replace('\n', '<br>', 'g');
+      element.value += text + "\n";
+      element.scrollTop = 99999; // focus on bottom
+    };
+  })(),
+  printErr: function(text) {
+    if (0) { // XXX disabled for safety typeof dump == 'function') {
+      dump(text + '\n'); // fast, straight to the real console
+    } else {
+      console.log(text);
+    }
+  },
+  canvas: document.getElementById('canvas'),
+  setStatus: function(text) {
+    if (Module.setStatus.interval) clearInterval(Module.setStatus.interval);
+    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+    var statusElement = document.getElementById('status');
+    var progressElement = document.getElementById('progress');
+    if (m) {
+      text = m[1];
+      progressElement.value = parseInt(m[2])*100;
+      progressElement.max = parseInt(m[4])*100;
+      progressElement.hidden = false;
+    } else {
+      progressElement.value = null;
+      progressElement.max = null;
+      progressElement.hidden = true;
+    }
+    statusElement.innerHTML = text;
+    if (text) {
+      var counter = 0;
+      Module.setStatus.interval = setInterval(function() {
+        counter++;
+        counter %= 3;
+        var dots = ' ';
+        for (var i = 0; i < counter; i++) dots += '.';
+        dots += '*';
+        for (var i = counter; i < 2; i++) dots += '.';
+        statusElement.innerHTML = text.replace('...', dots);
+      }, 300);
+    }
+  },
+  totalDependencies: 0,
+  monitorRunDependencies: function(left) {
+    this.totalDependencies = Math.max(this.totalDependencies, left);
+    Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+  }
+};
+Module.setStatus('Downloading...');
+
 // Loading music. Will be stopped once the first frame of the game runs
 
 Module.loadingMusic = new Audio();
