e4b334e26566a93f7f4c087dc37a974614fe05ff
Alon Zakai <alonzakai@gmail.com>
2013-01-28 10:41:55 -0800

    adjust windowed size

--- cube2/js/game-setup.js
+++ cube2/js/game-setup.js
@@ -310,7 +310,7 @@ Module.autoexec = function(){}; // called during autoexec on load, so useful to 
 Module.tweakDetail = function(){}; // called from postLoadWorld, so useful to make changes after the map has been loaded
 
 (function() {
-  var fraction = 0.70;
+  var fraction = 0.65;
   var desired = Math.min(fraction*screen.availWidth, fraction*screen.availHeight, 600);
   var w, h;
   if (screen.width >= screen.height) {
