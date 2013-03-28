bb02481e1a780abfaff5e89a8d5d1e92789f37d5
Alan Kligman <ack@mozilla.com>
2013-03-28 15:28:55 -0400

    Remove hardcoded app URL. Derp.

--- cube2/js/list.js
+++ cube2/js/list.js
@@ -57,7 +57,6 @@ var brokerUrl = 'http://mdsw.ch:8080';
 
 var pathname = window.location.pathname;
 var applicationUrl = window.location.hostname + pathname.substring(0, pathname.lastIndexOf('/'));
-applicationUrl = "developer.cdn.mozilla.net/media/uploads/demos/a/z/azakai/3baf4ad7e600cbda06ec46efec5ec3b8/bananabread_1364348082_demo_package";
 console.info(applicationUrl);
 
 if (window.location.search) {
