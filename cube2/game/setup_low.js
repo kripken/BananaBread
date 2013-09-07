5b1a14abe34097fbfd6932cc130c54e4e2b3b67f
Alon Zakai <alonzakai@gmail.com>
2013-09-06 17:51:13 -0700

    work towards showing video in-game

--- cube2/game/setup_low.js
+++ cube2/game/setup_low.js
@@ -11,6 +11,25 @@ Module.tweakDetail = function() {
     BananaBread.execute('shaderdetail 1');
     BananaBread.execute('maxdynlights 0');
   }
+
+  var video = document.createElement('video');
+  video.addEventListener("canplaythrough", function() {
+    video.play();
+    var glid = 0;
+    setInterval(function() {
+      if (!glid) glid = Module._getglid();
+      if (!glid) return;
+      var gl = Module.ctx;
+      gl.bindTexture(gl.TEXTURE_2D, GL.textures[glid]);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+    }, 1000/10);
+  }, true);
+  video.preload = 'auto';
+  video.loop = true;
+  video.src = 'Firefox.ogv';
+  video.crossOrigin = 'anonymous';
+  document.body.appendChild(video);
 };
 
 Module.loadDefaultMap = function() {
