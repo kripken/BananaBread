14cc3a5d0e32fad1f98a3984a1e2a6f2e4a8b979
Alon Zakai <alonzakai@gmail.com>
2013-09-10 16:07:45 -0700

    make video hidden on html

--- cube2/game/setup_low.js
+++ cube2/game/setup_low.js
@@ -12,41 +12,40 @@ Module.tweakDetail = function() {
     BananaBread.execute('maxdynlights 0');
   }
 
-///*
-  var boon = document.createElement('script');
-  boon.src = 'miniBoon.js';
-  document.body.appendChild(boon);
-//*/
-
-/*
-  // based on https://developer.mozilla.org/en-US/docs/Web/WebGL/Animating_textures_in_WebGL
-  var video = document.createElement('video');
-  video.addEventListener("canplaythrough", function() {
-    video.play();
-    var glid = 0;
-    setInterval(function() {
-      var first = false;
-      if (!glid) {
-        glid = Module._getglid();
-        if (glid) first = true;
-      }
-      if (!glid) return;
-      var gl = Module.ctx;
-      gl.bindTexture(gl.TEXTURE_2D, GL.textures[glid]);
-      if (first) {
-        // disable mipmap for this texture
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-      }
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
-    }, 1000/30);
-  }, true);
-  video.preload = 'auto';
-  video.loop = true;
-  video.src = 'Firefox.ogv';
-  video.crossOrigin = 'anonymous';
-  document.body.appendChild(video);
-*/
+  if (0) {
+    var boon = document.createElement('script');
+    boon.src = 'miniBoon.js';
+    document.body.appendChild(boon);
+  } else {
+    // based on https://developer.mozilla.org/en-US/docs/Web/WebGL/Animating_textures_in_WebGL
+    var video = document.createElement('video');
+    video.addEventListener("canplaythrough", function() {
+      video.play();
+      var glid = 0;
+      setInterval(function() {
+        var first = false;
+        if (!glid) {
+          glid = Module._getglid();
+          if (glid) first = true;
+        }
+        if (!glid) return;
+        var gl = Module.ctx;
+        gl.bindTexture(gl.TEXTURE_2D, GL.textures[glid]);
+        if (first) {
+          // disable mipmap for this texture
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        }
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+      }, 1000/30);
+    }, true);
+    video.preload = 'auto';
+    video.loop = true;
+    video.src = 'Firefox.ogv';
+    video.crossOrigin = 'anonymous';
+    video.style = 'display: none';
+    document.body.appendChild(video);
+  }
 };
 
 Module.loadDefaultMap = function() {
