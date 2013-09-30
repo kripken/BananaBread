4c54ee68d936f7b351bfcf8897add511aabed536
Alon Zakai <alonzakai@gmail.com>
2013-09-30 10:58:13 -0700

    tweets

--- cube2/game/setup_low.js
+++ cube2/game/setup_low.js
@@ -12,31 +12,104 @@ Module.tweakDetail = function() {
     BananaBread.execute('maxdynlights 0');
   }
 
+  var first = false;
+  var glid = 0;
+
+  function uploadTexture(element) {
+    if (!glid) {
+      glid = Module._getglid();
+      if (glid) first = true;
+    }
+    if (!glid) return false;
+    var gl = Module.ctx;
+    gl.bindTexture(gl.TEXTURE_2D, GL.textures[glid]);
+    if (first) {
+      // disable mipmap for this texture
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    }
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, element);
+    return true;
+  }
+
   if (1) {
+    var tweety = document.createElement('script');
+
+    tweety.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.width = 512;
+      canvas.height = 512;
+      var ctx = canvas.getContext("2d");
+
+      function render(text, callback) {
+        canvas.width = canvas.width; // clear it
+        var data = "<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512'>" +
+                     "<foreignObject width='100%' height='100%'>" +
+                       "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:16px; margin: 10px'>" +
+                         "<span>" + text + "</span>" + //  style='color:white; text-shadow:0 0 2px blue;'
+                       "</div>" +
+                     "</foreignObject>" +
+                   "</svg>";
+        var DOMURL = self.URL || self.webkitURL || self;
+        var img = new Image();
+        var svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
+        var url = DOMURL.createObjectURL(svg);
+        img.onload = function() {
+          ctx.drawImage(img, 0, 0);
+          DOMURL.revokeObjectURL(url);
+          callback(canvas);
+        };
+        img.src = url;
+      }
+
+      function getTweets(callback) {
+        function handleTweets(tweets){
+          var x = tweets.length;
+          var n = 0;
+          var html = '';
+          while(n < x) {
+            var tweet = tweets[n];
+            tweet = tweet.replace('class="user"', 'style="font-weight: bold"');
+            tweet = tweet.replace('class="timePosted"', 'style="font-style: italic"');
+            console.log(JSON.stringify(tweet, null, '  '));
+            html += '<p>' + tweet + '</p><hr></hr>';
+            n++;
+          }
+          html += '';
+          callback(html);
+        }
+        twitterFetcher.fetch('384367035376873472', 'example1', 5, false, true, true, '', false, handleTweets, false);
+      }
+
+      getTweets(function(html) {
+        html = html.replace(/[\n'&]/g, '');
+        for (var i = 0; i < 20; i++) html = html.replace(/  /g, ' ')
+        render(html, function doRender(canvas) {
+          if (!uploadTexture(canvas)) {
+            setTimeout(function() {
+              doRender(canvas);
+            }, 500);
+          }
+        });
+      });
+    };
+
+    tweety.src = 'tweety.js';
+    document.body.appendChild(tweety);
+
+  } else if (0) {
+    // boon
     var boon = document.createElement('script');
     boon.src = 'miniBoon.js';
     document.body.appendChild(boon);
   } else {
+    // video
     // based on https://developer.mozilla.org/en-US/docs/Web/WebGL/Animating_textures_in_WebGL
     var video = document.createElement('video');
     video.addEventListener("canplaythrough", function() {
       video.play();
-      var glid = 0;
       setInterval(function() {
-        var first = false;
-        if (!glid) {
-          glid = Module._getglid();
-          if (glid) first = true;
-        }
-        if (!glid) return;
-        var gl = Module.ctx;
-        gl.bindTexture(gl.TEXTURE_2D, GL.textures[glid]);
-        if (first) {
-          // disable mipmap for this texture
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        }
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+        uploadTexture(video);
       }, 1000/30);
     }, true);
     video.preload = 'auto';
---
+++ cube2/tweety.js
@@ -0,0 +1,81 @@
+/*********************************************************************
+*  #### Twitter Post Fetcher v10.0 ####
+*  Coded by Jason Mayes 2013. A present to all the developers out there.
+*  www.jasonmayes.com
+*  Please keep this disclaimer with my code if you use it. Thanks. :-)
+*  Got feedback or questions, ask here: 
+*  http://www.jasonmayes.com/projects/twitterApi/
+*  Updates will be posted to this site.
+*********************************************************************/
+var twitterFetcher=function(){function x(e){return e.replace(/<b[^>]*>(.*?)<\/b>/gi,function(c,e){return e}).replace(/class=".*?"|data-query-source=".*?"|dir=".*?"|rel=".*?"/gi,"")}function p(e,c){for(var g=[],f=RegExp("(^| )"+c+"( |$)"),a=e.getElementsByTagName("*"),h=0,d=a.length;h<d;h++)f.test(a[h].className)&&g.push(a[h]);return g}var y="",l=20,s=!0,k=[],t=!1,q=!0,r=!0,u=null,v=!0,z=!0,w=null,A=!0;return{fetch:function(e,c,g,f,a,h,d,b,m,n){void 0===g&&(g=20);void 0===f&&(s=!0);void 0===a&&(a=
+!0);void 0===h&&(h=!0);void 0===d&&(d="default");void 0===b&&(b=!0);void 0===m&&(m=null);void 0===n&&(n=!0);t?k.push({id:e,domId:c,maxTweets:g,enableLinks:f,showUser:a,showTime:h,dateFunction:d,showRt:b,customCallback:m,showInteraction:n}):(t=!0,y=c,l=g,s=f,r=a,q=h,z=b,u=d,w=m,A=n,c=document.createElement("script"),c.type="text/javascript",c.src="//cdn.syndication.twimg.com/widgets/timelines/"+e+"?&lang=en&callback=twitterFetcher.callback&suppress_response_codes=true&rnd="+Math.random(),document.getElementsByTagName("head")[0].appendChild(c))},
+callback:function(e){var c=document.createElement("div");c.innerHTML=e.body;"undefined"===typeof c.getElementsByClassName&&(v=!1);e=[];var g=[],f=[],a=[],h=[],d=0;if(v)for(c=c.getElementsByClassName("tweet");d<c.length;){0<c[d].getElementsByClassName("retweet-credit").length?a.push(!0):a.push(!1);if(!a[d]||a[d]&&z)e.push(c[d].getElementsByClassName("e-entry-title")[0]),h.push(c[d].getAttribute("data-tweet-id")),g.push(c[d].getElementsByClassName("p-author")[0]),f.push(c[d].getElementsByClassName("dt-updated")[0]);
+d++}else for(c=p(c,"tweet");d<c.length;)e.push(p(c[d],"e-entry-title")[0]),h.push(c[d].getAttribute("data-tweet-id")),g.push(p(c[d],"p-author")[0]),f.push(p(c[d],"dt-updated")[0]),0<p(c[d],"retweet-credit").length?a.push(!0):a.push(!1),d++;e.length>l&&(e.splice(l,e.length-l),g.splice(l,g.length-l),f.splice(l,f.length-l),a.splice(l,a.length-l));c=[];d=e.length;for(a=0;a<d;){if("string"!==typeof u){var b=new Date(f[a].getAttribute("datetime").replace(/-/g,"/").replace("T"," ").split("+")[0]),b=u(b);
+f[a].setAttribute("aria-label",b);if(e[a].innerText)if(v)f[a].innerText=b;else{var m=document.createElement("p"),n=document.createTextNode(b);m.appendChild(n);m.setAttribute("aria-label",b);f[a]=m}else f[a].textContent=b}b="";s?(r&&(b+='<div class="user">'+x(g[a].innerHTML)+"</div>"),b+='<p class="tweet">'+x(e[a].innerHTML)+"</p>",q&&(b+='<p class="timePosted">'+f[a].getAttribute("aria-label")+"</p>")):e[a].innerText?(r&&(b+='<p class="user">'+g[a].innerText+"</p>"),b+='<p class="tweet">'+e[a].innerText+
+"</p>",q&&(b+='<p class="timePosted">'+f[a].innerText+"</p>")):(r&&(b+='<p class="user">'+g[a].textContent+"</p>"),b+='<p class="tweet">'+e[a].textContent+"</p>",q&&(b+='<p class="timePosted">'+f[a].textContent+"</p>"));A&&(b+='<p class="interact"><a href="https://twitter.com/intent/tweet?in_reply_to='+h[a]+'" class="twitter_reply_icon">Reply</a><a href="https://twitter.com/intent/retweet?tweet_id='+h[a]+'" class="twitter_retweet_icon">Retweet</a><a href="https://twitter.com/intent/favorite?tweet_id='+
+h[a]+'" class="twitter_fav_icon">Favorite</a></p>');c.push(b);a++}if(null==w){e=c.length;g=0;f=document.getElementById(y);for(h="<ul>";g<e;)h+="<li>"+c[g]+"</li>",g++;f.innerHTML=h+"</ul>"}else w(c);t=!1;0<k.length&&(twitterFetcher.fetch(k[0].id,k[0].domId,k[0].maxTweets,k[0].enableLinks,k[0].showUser,k[0].showTime,k[0].dateFunction,k[0].showRt,k[0].customCallback,k[0].showInteraction),k.splice(0,1))}}}();
+
+
+/*
+* ### HOW TO CREATE A VALID ID TO USE: ###
+* Go to www.twitter.com and sign in as normal, go to your settings page.
+* Go to "Widgets" on the left hand side.
+* Create a new widget for what you need eg "user timeline" or "search" etc. 
+* Feel free to check "exclude replies" if you dont want replies in results.
+* Now go back to settings page, and then go back to widgets page, you should
+* see the widget you just created. Click edit.
+* Now look at the URL in your web browser, you will see a long number like this:
+* 345735908357048478
+* Use this as your ID below instead!
+*/
+
+/**
+ * How to use fetch function:
+ * @param {string} Your Twitter widget ID.
+ * @param {string} The ID of the DOM element you want to write results to. 
+ * @param {int} Optional - the maximum number of tweets you want returned. Must
+ *     be a number between 1 and 20.
+ * @param {boolean} Optional - set true if you want urls and hash
+       tags to be hyperlinked!
+ * @param {boolean} Optional - Set false if you dont want user photo /
+ *     name for tweet to show.
+ * @param {boolean} Optional - Set false if you dont want time of tweet
+ *     to show.
+ * @param {function/string} Optional - A function you can specify to format
+ *     tweet date/time however you like. This function takes a JavaScript date
+ *     as a parameter and returns a String representation of that date.
+ *     Alternatively you may specify the string 'default' to leave it with
+ *     Twitter's default renderings.
+ * @param {boolean} Optional - Show retweets or not. Set false to not show.
+ * @param {function/string} Optional - A function to call when data is ready. It
+ *     also passes the data to this function should you wish to manipulate it
+ *     yourself before outputting. If you specify this parameter you  must
+ *     output data yourself!
+ * @param {boolean} Optional - Show links for reply, retweet, favourite. Set false to not show.
+ */
+
+// ##### Simple example 1 #####
+// A simple example to get my latest tweet and write to a HTML element with
+// id "tweets". Also automatically hyperlinks URLS and user mentions and
+// hashtags.
+/*
+twitterFetcher.fetch('384367035376873472', 'example1', 5, false, true, true, '', false, handleTweets, false);
+
+function handleTweets(tweets){
+    var x = tweets.length;
+    var n = 0;
+    var element = document.getElementById('example1');
+    var html = '<ul>';
+    while(n < x) {
+      var tweet = tweets[n];
+      tweet = tweet.replace('class="user"', 'style="font-weight: bold"');
+      tweet = tweet.replace('class="timePosted"', 'style="font-style: italic"');
+      console.log(JSON.stringify(tweet, null, '  '));
+      html += '<li>' + tweet + '</li>';
+      n++;
+    }
+    html += '</ul>';
+    element.innerHTML = html;
+}
+*/
+
