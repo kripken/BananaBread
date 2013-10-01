0dde7a930838ac5d6cb6047ac890d2a770ff002d
Alon Zakai <alonzakai@gmail.com>
2013-09-30 17:08:22 -0700

    add a border

--- cube2/game/setup_low.js
+++ cube2/game/setup_low.js
@@ -50,7 +50,7 @@ Module.tweakDetail = function() {
         canvas.width = canvas.width; // clear it
         var data = "<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512'>" +
                      "<foreignObject width='100%' height='100%'>" +
-                       "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:18px; background-color: #200C05'>" +
+                       "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:18px; border: 10px solid white; background-color: #200C05'>" +
                          "<span style='margin-left: 10px; color: #ff9955; text-shadow:0 0 3px red;'>" + text + "</span>" + //  style=''
                        "</div>" +
                      "</foreignObject>" +
