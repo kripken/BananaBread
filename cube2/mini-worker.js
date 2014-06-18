be897461c20152029819b83dc25212b167864804
Alon Zakai <alonzakai@gmail.com>
2014-06-18 11:28:24 -0700

    add mini-worker.html|js files

---
+++ cube2/mini-worker.js
@@ -0,0 +1,10 @@
+
+importScripts('js/api.js');
+importScripts('js/zee.js');
+importScripts('game/gl-matrix.js.js');
+importScripts('game/setup_low.js');
+importScripts('game/preload_base.js');
+importScripts('game/preload_character.js');
+importScripts('game/preload_low.js');
+importScripts('bb.js');
+
