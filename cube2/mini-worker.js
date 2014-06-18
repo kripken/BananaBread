2a6237eeee5e95fa3ece3a14fd5ff8e19fd6ea9f
Alon Zakai <alonzakai@gmail.com>
2014-06-18 13:44:08 -0700

    fix up mini-worker

--- cube2/mini-worker.js
+++ cube2/mini-worker.js
@@ -1,10 +1,14 @@
 
-importScripts('js/api.js');
-importScripts('js/zee.js');
-importScripts('game/gl-matrix.js.js');
-importScripts('game/setup_low.js');
+importScripts('game/gl-matrix.js');
+
 importScripts('game/preload_base.js');
 importScripts('game/preload_character.js');
 importScripts('game/preload_low.js');
-importScripts('bb.js');
+
+importScripts('game/setup_low.js');
+
+importScripts('js/api.js');
+importScripts('js/zee.js');
+
+importScripts('bb.worker.js');
 
