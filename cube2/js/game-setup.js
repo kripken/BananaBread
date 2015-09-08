5bede143580aa3211cfcc81e1eb5ba789f9a7f53
Alon Zakai <alonzakai@gmail.com>
2015-09-08 15:11:08 -0700

    remove zee.js usage, just use zlib normally

--- cube2/js/game-setup.js
+++ cube2/js/game-setup.js
@@ -197,45 +197,6 @@ Module.loadingMusic.play();
 Module.readySound = new Audio();
 Module.readySound.src = 'assets/alarmcreatemiltaryfoot_1.ogg';
 
-// Pre-unzip ogz files, we can do this in parallel in a worker during preload
-
-(function() {
-  // TODO: use zee.js
-  var zeeWorker = new Worker('game/zee-worker.js');
-
-  var zeeCallbacks = [];
-
-  zeeWorker.onmessage = function(msg) {
-    zeeCallbacks[msg.data.callbackID](msg.data.data);
-    console.log("zee'd " + msg.data.filename + ' in ' + msg.data.time + ' ms, ' + msg.data.data.length + ' bytes');
-    zeeCallbacks[msg.data.callbackID] = null;
-  };
-
-  function requestZee(filename, data, callback) {
-    zeeWorker.postMessage({
-      filename: filename,
-      data: new Uint8Array(data), // do not send over the underlying ArrayBuffer
-      callbackID: zeeCallbacks.length
-    });
-    zeeCallbacks.push(callback);
-  }
-
-  Module.postRun.push(function() {
-    zeeWorker.terminate();
-  });
-
-  Module.preloadPlugins.push({
-    canHandle: function(name) {
-      return name.substr(-4) == '.ogz';
-    },
-    handle: function(byteArray, name, onload, onerror) {
-      requestZee(name, byteArray, function(byteArray) {
-        onload(byteArray);
-      });
-    }
-  });
-})();
-
 // Hooks
 
 Module.setOpacity = function(opacity) {
